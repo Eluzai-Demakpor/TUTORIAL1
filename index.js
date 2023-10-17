9a937ead1f73fedd539f24ea483dde999627283c
Eluzai Demakpor <eluzaidemakpor@gmail.com>
2023-10-17 11:35:18 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.getElementById("count").innerText = 5
+
+/*let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+console.log(count) */
+
+let myAge = 20
+let humanDogRatio = 7
+let myDogAge = myAge*humanDogRatio
+console.log(myDogAge)
